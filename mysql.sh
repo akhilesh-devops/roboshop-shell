f270082c6343bc04ba3699e6d99e939ae9830a2f
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-15 21:18:00 +0530

    shell-script

--- mysql.sh
+++ mysql.sh
@@ -4,25 +4,27 @@ if [ -z "${mysql_root_password}" ]; then
   exit 1
 fi
 
+log=/tmp/roboshop.log
+
 source common.sh
 
 echo -e "\e[36m>>>>>>>>>>>>>>>> Create mysql repo File <<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>${log}
 func_exit_status
 
 echo -e "\e[36m>>>>>>>>>>>>>>>> Disable mysql module <<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-yum module disable mysql -y
+yum module disable mysql -y &>>${log}
 func_exit_status
 
 echo -e "\e[36m>>>>>>>>>>>>>>>> Install mysql  <<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-yum install mysql-community-server -y
+yum install mysql-community-server -y &>>${log}
 func_exit_status
 
 echo -e "\e[36m>>>>>>>>>>>>>>>> Restart the service <<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-systemctl enable mysqld
-systemctl start mysqld
+systemctl enable mysqld &>>${log}
+systemctl start mysqld &>>${log}
 func_exit_status
 
 echo -e "\e[36m>>>>>>>>>>>>>>>> Set Password <<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
-mysql_secure_installation --set-root-pass ${mysql_root_password}
+mysql_secure_installation --set-root-pass ${mysql_root_password} &>>${log}
 func_exit_status
