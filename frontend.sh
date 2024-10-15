92e446ad3049c59bc574eb5a7b942f6786012070
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-15 11:01:41 +0530

    shell-script

--- frontend.sh
+++ frontend.sh
@@ -1,19 +1,19 @@
-echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>> install Nginx <<<<<<<<<<<<<<<<<<<<<<<</e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>> install Nginx <<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 yum install nginx -y
 
-echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>> copy the roboshop configuration <<<<<<<<<<<<<<<<<<<<<<<</e[0m"
-cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>> copy the roboshop configuration <<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+cp nginx-roboshop.conf \etc/nginx/default.d/roboshop.conf
 
-echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>> cleaning the old content <<<<<<<<<<<<<<<<<<<<<<<</e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>> cleaning the old content <<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/*
 
-echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>> Download application content <<<<<<<<<<<<<<<<<<<<<<<</e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>> Download application content <<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
-echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>> Extract application content <<<<<<<<<<<<<<<<<<<<<<<</e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>> Extract application content <<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-echo -e "/e[36m>>>>>>>>>>>>>>>>>>>>> Start the service <<<<<<<<<<<<<<<<<<<<<<<</e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>> Start the service <<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl enable nginx
 systemctl restart nginx
