6c7972be8b01ab75c4d53f1b72c188f850df1d9c
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-15 23:13:01 +0530

    shell-script

--- mysql.sh
+++ mysql.sh
@@ -3,23 +3,23 @@ log=/tmp/roboshop.log
 
 source common.sh
 
-echo -e "\e[36m>>>>>>>>>>>>>>>> Create mysql repo File <<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>> Create mysql repo File <<<<<<<<<<<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>${log}
 func_exit_status
 
-echo -e "\e[36m>>>>>>>>>>>>>>>> Disable mysql module <<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>> Disable mysql module <<<<<<<<<<<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
 yum module disable mysql -y &>>${log}
 func_exit_status
 
-echo -e "\e[36m>>>>>>>>>>>>>>>> Install mysql  <<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>> Install mysql  <<<<<<<<<<<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
 yum install mysql-community-server -y &>>${log}
 func_exit_status
 
-echo -e "\e[36m>>>>>>>>>>>>>>>> Restart the service <<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>> Restart the service <<<<<<<<<<<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
 systemctl enable mysqld &>>${log}
 systemctl start mysqld &>>${log}
 func_exit_status
 
-echo -e "\e[36m>>>>>>>>>>>>>>>> Set Password <<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+echo -e "\e[36m>>>>>>>>>>>>>>>> Set Password <<<<<<<<<<<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
 mysql_secure_installation --set-root-pass RoboShop@1 &>>${log}
 func_exit_status
