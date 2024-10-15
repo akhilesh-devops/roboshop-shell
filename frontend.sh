67e7747f688f028ac69cfe5efe8cc1ac4b359ac3
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-15 10:48:01 +0530

    shell-script

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,10 @@
-
+echo -e "/e[32m>>>>>>>>>>>>>>>>>>>>> install Nginx <<<<<<<<<<<<<<<<<<<<<<<</e[0m"
 yum install nginx -y
 
+echo -e "/e[32m>>>>>>>>>>>>>>>>>>>>> copy the roboshop configuration <<<<<<<<<<<<<<<<<<<<<<<</e[0m"
 cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
+echo -e "/e[32m>>>>>>>>>>>>>>>>>>>>> cleaning the old content <<<<<<<<<<<<<<<<<<<<<<<</e[0m"
 rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
