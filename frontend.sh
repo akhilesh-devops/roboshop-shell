4f8c550e5cf8b4f8b9a7d0a7e66e14af174dcb6d
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-15 10:57:15 +0530

    shell-script

--- frontend.sh
+++ frontend.sh
@@ -7,12 +7,13 @@ cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 echo -e "/e[32m>>>>>>>>>>>>>>>>>>>>> cleaning the old content <<<<<<<<<<<<<<<<<<<<<<<</e[0m"
 rm -rf /usr/share/nginx/html/*
 
+echo -e "/e[32m>>>>>>>>>>>>>>>>>>>>> Download application content <<<<<<<<<<<<<<<<<<<<<<<</e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
+echo -e "/e[32m>>>>>>>>>>>>>>>>>>>>> Extract application content <<<<<<<<<<<<<<<<<<<<<<<</e[0m"
 cd /usr/share/nginx/html
-
 unzip /tmp/frontend.zip
 
+echo -e "/e[32m>>>>>>>>>>>>>>>>>>>>> Start the service <<<<<<<<<<<<<<<<<<<<<<<</e[0m"
 systemctl enable nginx
-
 systemctl restart nginx
