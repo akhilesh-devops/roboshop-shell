9670ffac6af9522b371f12b7be92de2be2737dc4
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-15 21:03:28 +0530

    shell-script

--- cart.sh
+++ cart.sh
@@ -1,17 +1,4 @@
-cp cart.service /etc/systemd/system/cart.service
+component=cart
+source common.sh
 
-yum module disable nodejs -y
-yum module enable nodejs:18 -y
-yum install nodejs -y
-useradd roboshop
-mkdir /app
-curl -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
-cd /app
-unzip /tmp/cart.zip
-cd /app
-npm install
-
-
-systemctl daemon-reload
-systemctl enable cart
-systemctl restart cart
+nodejs
