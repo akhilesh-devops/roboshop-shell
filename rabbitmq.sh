ac27f0897414b12407eba4725cbce8ea9e7daffa
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-15 23:38:08 +0530

    shell-script

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,7 +1,28 @@
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
-dnf install rabbitmq-server -y
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+log=/tmp/roboshop.log
+
+source common.sh
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Create the Rabbitmq repos <<<<<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>${log}
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Download the Rabbitmq repos <<<<<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>${log}
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Install rabbitmq server <<<<<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+dnf install rabbitmq-server -y &>>${log}
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Restart the service <<<<<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+systemctl enable rabbitmq-server &>>${log}
+systemctl start rabbitmq-server &>>${log}
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Adding user and password <<<<<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+rabbitmqctl add_user roboshop roboshop123 &>>${log}
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>>>>>>> Setting permissions <<<<<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>${log}
+func_exit_status
