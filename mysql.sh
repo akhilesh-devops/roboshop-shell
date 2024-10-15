d0b7916a1a30e9f69c2b82440ef488b8d7b81f9d
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-15 21:13:28 +0530

    shell-script

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,28 @@
+mysql_root_password=$1
+if [ -z "${mysql_root_password}" ]; then
+  echo INput Password Missing
+  exit 1
+fi
+
+source common.sh
+
+echo -e "\e[36m>>>>>>>>>>>>>>>> Create mysql repo File <<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 cp mysql.repo /etc/yum.repos.d/mysql.repo
-dnf module disable mysql -y
-dnf install mysql-community-server -y
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>>>>>>> Disable mysql module <<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+yum module disable mysql -y
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>>>>>>> Install mysql  <<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+yum install mysql-community-server -y
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>>>>>>> Restart the service <<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl enable mysqld
 systemctl start mysqld
-mysql_secure_installation --set-root-pass RoboShop@1
+func_exit_status
+
+echo -e "\e[36m>>>>>>>>>>>>>>>> Set Password <<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
+mysql_secure_installation --set-root-pass ${mysql_root_password}
+func_exit_status
