62383ef9098ff74a5628e70d856932f459a98dea
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-15 11:56:53 +0530

    shell-script

--- redis.sh
+++ redis.sh
@@ -1,7 +1,18 @@
-dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
-dnf module enable redis:remi-6.2 -y
-dnf install redis -y
-sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf
+log=/tmp/roboshop.log
 
-systemctl enable redis
-systemctl restart redis
+echo -e "\e[36m>>>>>>>>>>>>>>>>>> Download remi repo file <<<<<<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>${log}
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>>> Enable redis module <<<<<<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+dnf module enable redis:remi-6.2 -y &>>${log}
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>>> Install redis <<<<<<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+dnf install redis -y &>>${log}
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>>> Update the config file <<<<<<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis.conf /etc/redis/redis.conf &>>${log}
+
+
+echo -e "\e[36m>>>>>>>>>>>>>>>>>> Restart the service <<<<<<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+systemctl enable redis &>>${log}
+systemctl restart redis &>>${log}
