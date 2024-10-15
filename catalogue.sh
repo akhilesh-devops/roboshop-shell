6cebc2afe5724375a402cf416ac2a4c8e1a97d99
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-15 10:04:55 +0530

    shell-script

--- catalogue.sh
+++ catalogue.sh
@@ -1,18 +1,28 @@
+
 cp catalogue.service /etc/systemd/system/catalogue.service
+
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 yum module disable nodejs -y
+
 yum module enable nodejs:18 -y
+
 yum install nodejs -y
+
 useradd roboshop
+
 mkdir /app
+
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+
 cd /app
 unzip /tmp/catalogue.zip
+
 cd /app
 npm install
 
 yum install mongodb-org-shell -y
+
 mongo --host mongodb.vinithaws.online </app/schema/catalogue.js
 
 systemctl daemon-reload
