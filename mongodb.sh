eaa93bf6afb78cfaccf0bd5772d72ba84e4e674e
AKhilesh <kothapallyakhil123@gmail.com>
2024-12-27 00:08:43 +0530

    test

--- mongodb.sh
+++ mongodb.sh
@@ -1,14 +1,18 @@
-log=/tmp/roboshop.log
+source common.sh
 
 echo -e "\e[36m >>>>>>>>>>>>> copy Mongo repo file <<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
 cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
+func_exit_status
 
 echo -e "\e[36m >>>>>>>>>>>> Install Mongodb <<<<<<<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
 dnf install mongodb-org -y &>>${log}
+func_exit_status
 
 echo -e "\e[36m >>>>>>>>>>>> Update Listen address <<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>${log}
+func_exit_status
 
 echo -e "\e[36m >>>>>>>>>>> Restart the mongodb service <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
 systemctl enable mongod &>>${log}
 systemctl restart mongod &>>${log}
+func_exit_status
