52920d1b869617c51e76eaa9647215fef54349fc
AKhilesh <kothapallyakhil123@gmail.com>
2024-12-27 00:29:15 +0530

    test

--- frontend.sh
+++ frontend.sh
@@ -28,6 +28,6 @@ func_exit_status
 
 echo -e "\e[36m>>>>>>>>>>>>  Start Nginx Service  <<<<<<<<<<<<\e[0m"
 systemctl enable nginx &>>${log}
-systemctl restart nginx &>>${log}
+systemctl start nginx &>>${log}
 func_exit_status
 
