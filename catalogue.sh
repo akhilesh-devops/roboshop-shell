342b167634f6c047ed3b652c1df6bcae5f9d491c
AKhilesh <kothapallyakhil123@gmail.com>
2024-12-26 14:36:26 +0530

    test

--- catalogue.sh
+++ catalogue.sh
@@ -12,7 +12,7 @@ unzip /tmp/catalogue.zip
 npm install
 
 dnf install mongodb-org-shell -y
-mongo --host MONGODB-SERVER-IPADDRESS </app/schema/catalogue.js
+mongo --host mongodb.vinithaws.online </app/schema/catalogue.js
 
 systemctl daemon-reload
 systemctl enable catalogue
