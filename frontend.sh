1d2b1bf1f03dd41f2c0a81a37b0c4bec49cda77d
AKhilesh <kothapallyakhil123@gmail.com>
2024-12-26 22:51:28 +0530

    test

--- frontend.sh
+++ frontend.sh
@@ -1,8 +1,17 @@
+echo -e "\e[32m >>>>>>>>>>>>>>>>> Copy Roboshop configuration <<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 cp nginx-roboshop.conf /etc/nginx/default.d/nginx-roboshop.conf
+
+echo -e "\e[32m >>>>>>>>>>>>>>>>> Install Nginx <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 dnf install nginx -y
+
+echo -e "\e[32m >>>>>>>>>>>>>>>>> Remove the app content <<<<<<<<<<<<<<<<<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/*
+
+echo -e "\e[32m >>>>>>>>>>>>>>>> Download and extract the app content <<<<<<<<<<<<<<<<\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+
+echo -e "\e[32m >>>>>>>>>>>>>>> Restart the Nginx service <<<<<<<<<<<<<<<<<<<\e[0m"
 systemctl restart nginx
 systemctl enable nginx
