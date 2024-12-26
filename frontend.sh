56e4755297d7ba21ed0e65dd1fc56b38b08b8423
AKhilesh <kothapallyakhil123@gmail.com>
2024-12-26 23:08:13 +0530

    test

--- frontend.sh
+++ frontend.sh
@@ -1,20 +1,20 @@
 log = /tmp/roboshop.log
 
-echo -e "\e[32m >>>>>>>>>>>>>>>>> Copy Nginx configuration <<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
-cp nginx-roboshop.conf /etc/nginx/default.d/nginx-roboshop.conf &>>${log}
+echo -e "\e[32m >>>>>>>>>>>>>>>>> Copy Nginx configuration <<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
+cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log}
 
-echo -e "\e[32m >>>>>>>>>>>>>>>>> Install Nginx <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+echo -e "\e[32m >>>>>>>>>>>>>>>>> Install Nginx <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
 dnf install nginx -y &>>${log}
 
-echo -e "\e[32m >>>>>>>>>>>>>>>>> Remove the app content <<<<<<<<<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+echo -e "\e[32m >>>>>>>>>>>>>>>>> Remove the app content <<<<<<<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
 rm -rf /usr/share/nginx/html/* &>>${log}
 
-echo -e "\e[32m >>>>>>>>>>>>>>>> Download and extract the app content <<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+echo -e "\e[32m >>>>>>>>>>>>>>>> Download and extract the app content <<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>> ${log}
 cd /usr/share/nginx/html &>>${log}
 unzip /tmp/frontend.zip &>>${log}
 
-echo -e "\e[32m >>>>>>>>>>>>>>> Restart the Nginx service <<<<<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+echo -e "\e[32m >>>>>>>>>>>>>>> Restart the Nginx service <<<<<<<<<<<<<<<<<<<\e[0m" | tee -a /tmp/roboshop.log
 systemctl restart nginx &>>${log}
 systemctl enable nginx &>>${log}
 
