75c46c6107e6116c9bcfe5fe73ac11016042e1e6
AKhilesh <kothapallyakhil123@gmail.com>
2025-01-10 14:08:04 +0530

    test

--- frontend.sh
+++ frontend.sh
@@ -1,24 +1,25 @@
+log=/tmp/roboshop.log
 
 echo -e "\e[36m>>>>>>>>>>> Install Nginx <<<<<<<<<<<<\e[0m" 
-yum install nginx -y 
+yum install nginx -y &>>${log}
 
 echo -e "\e[36m>>>>>>>>>> copy roboshop config <<<<<<<<<<<\e[0m" 
-cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf 
+cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log}
 
 echo -e "\e[36m>>>>>>>>>> Remove app content <<<<<<<<<<<\e[0m" 
-rm -rf /usr/share/nginx/html/* 
+rm -rf /usr/share/nginx/html/* &>>${log}
 
 echo -e "\e[36m>>>>>>>>>> Download the application content <<<<<<<<<<<<<\e[0m" 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip 
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
 
 echo -e "\e[36m>>>>>>>>> change the directory <<<<<<<<<<<<<\e[0m" 
-cd /usr/share/nginx/html 
+cd /usr/share/nginx/html &>>${log}
 
 echo -e "\e[36m>>>>>>>> unzip the app content <<<<<<<<<<<<<\e[0m" 
-unzip /tmp/frontend.zip 
+unzip /tmp/frontend.zip &>>${log}
 
 echo -e "\e[36m>>>>>>> Restart the service <<<<<<<<<<<\e[0m" 
-systemctl enable nginx 
-systemctl restart nginx 
+systemctl enable nginx &>>${log}
+systemctl restart nginx &>>${log}
 
 
