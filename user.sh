144040e9df502f2c91b599dc9ab1209157cbc91d
AKhilesh <kothapallyakhil123@gmail.com>
2025-01-10 14:46:26 +0530

    test

--- user.sh
+++ user.sh
@@ -1,15 +1,5 @@
-cp user.service /etc/systemd/system/user.service
-dnf module disable nodejs -y
-dnf module enable nodejs:18 -y
-dnf install nodejs -y
-useradd roboshop
-mkdir /app
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
-cd /app
-unzip /tmp/user.zip
-cd /app
-npm install
-systemctl daemon-reload
-systemctl enable user
-systemctl start user
+component=user
+source common.sh
+
+func_nodejs
 
