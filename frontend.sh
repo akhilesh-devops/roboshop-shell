599950c064e0bec3a4f6745cb68133be54727b62
AKhilesh <kothapallyakhil123@gmail.com>
2025-01-10 14:11:36 +0530

    test

--- frontend.sh
+++ frontend.sh
@@ -1,24 +1,24 @@
 log=/tmp/roboshop.log
 
-echo -e "\e[36m>>>>>>>>>>> Install Nginx <<<<<<<<<<<<\e[0m" | tee -a ${log}
+echo -e "\e[36m>>>>>>>>>>> Install Nginx <<<<<<<<<<<<\e[0m" | tee -a &>>${log}
 yum install nginx -y &>>${log}
 
-echo -e "\e[36m>>>>>>>>>> copy roboshop config <<<<<<<<<<<\e[0m" | tee -a ${log}
+echo -e "\e[36m>>>>>>>>>> copy roboshop config <<<<<<<<<<<\e[0m" | tee -a &>>${log}
 cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log}
 
-echo -e "\e[36m>>>>>>>>>> Remove app content <<<<<<<<<<<\e[0m" | tee -a ${log}
+echo -e "\e[36m>>>>>>>>>> Remove app content <<<<<<<<<<<\e[0m" | tee -a &>>${log}
 rm -rf /usr/share/nginx/html/* &>>${log}
 
-echo -e "\e[36m>>>>>>>>>> Download the application content <<<<<<<<<<<<<\e[0m" | tee -a ${log}
+echo -e "\e[36m>>>>>>>>>> Download the application content <<<<<<<<<<<<<\e[0m" | tee -a &>>${log}
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
 
-echo -e "\e[36m>>>>>>>>> change the directory <<<<<<<<<<<<<\e[0m" | tee -a ${log}
+echo -e "\e[36m>>>>>>>>> change the directory <<<<<<<<<<<<<\e[0m" | tee -a &>>${log}
 cd /usr/share/nginx/html &>>${log}
 
-echo -e "\e[36m>>>>>>>> unzip the app content <<<<<<<<<<<<<\e[0m" | tee -a ${log}
+echo -e "\e[36m>>>>>>>> unzip the app content <<<<<<<<<<<<<\e[0m" | tee -a &>>${log}
 unzip /tmp/frontend.zip &>>${log}
 
-echo -e "\e[36m>>>>>>> Restart the service <<<<<<<<<<<\e[0m" | tee -a ${log}
+echo -e "\e[36m>>>>>>> Restart the service <<<<<<<<<<<\e[0m" | tee -a &>>${log}
 systemctl enable nginx &>>${log}
 systemctl restart nginx &>>${log}
 
