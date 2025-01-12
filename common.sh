e2f3ab01a9b46dc66708f15325ea830328b93716
AKhilesh <kothapallyakhil123@gmail.com>
2025-01-12 11:13:04 +0530

    test

--- common.sh
+++ common.sh
@@ -8,84 +8,51 @@ func_exit_status() {
   fi
 }
 
-
 func_apppreq() {
-
-  echo -e "\e[36m >>>>>>>>>>>> create ${component} service file <<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+  echo -e "\e[36m>>>>>>>>>>>>  Create ${component} Service  <<<<<<<<<<<<\e[0m"
   cp ${component}.service /etc/systemd/system/${component}.service &>>${log}
   func_exit_status
 
-  echo -e "\e[36m >>>>>>>>>>>>>>> create application user <<<<<<<<<<<<<\e[0m" | tee -a ${log}
+  echo -e "\e[36m>>>>>>>>>>>>  Create Application User  <<<<<<<<<<<<\e[0m"
   id roboshop &>>${log}
   if [ $? -ne 0 ]; then
     useradd roboshop &>>${log}
   fi
   func_exit_status
 
-  echo -e "\e[36m >>>>>>>>>>>>>> cleanup the app directory <<<<<<<<<<<\e[0m" | tee -a ${log}
+  echo -e "\e[36m>>>>>>>>>>>>  Cleanup Existing Application Content  <<<<<<<<<<<<\e[0m"
   rm -rf /app &>>${log}
   func_exit_status
 
-  echo -e "\e[36m >>>>>>>>>>>>> create app directory <<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+  echo -e "\e[36m>>>>>>>>>>>>  Create Application Directory  <<<<<<<<<<<<\e[0m"
   mkdir /app &>>${log}
   func_exit_status
 
-  echo -e "\e[36m >>>>>>>>>>>> Download the app content <<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+  echo -e "\e[36m>>>>>>>>>>>>  Download Application Content  <<<<<<<<<<<<\e[0m"
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log}
   func_exit_status
 
-  echo -e "\e[36m >>>>>>>>>>>> Extract the app content <<<<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
-  cd /app &>>${log}
+  echo -e "\e[36m>>>>>>>>>>>>  Extract Application Content  <<<<<<<<<<<<\e[0m"
+  cd /app
   unzip /tmp/${component}.zip &>>${log}
   func_exit_status
 }
 
-func_nodejs() {
-
-  echo -e "\e[36m >>>>>>>>>>>> create mongo repo <<<<<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
-  cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
-  func_exit_status
-
-  echo -e "\e[36m >>>>>>>>>>> Disable the nodejs module <<<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
-  dnf module disable nodejs -y &>>${log}
-  func_exit_status
-
-  echo -e "\e[36m >>>>>>>>>>> Enable the nodejs module <<<<<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
-  dnf module enable nodejs:18 -y &>>${log}
-  func_exit_status
-
-  echo -e "\e[36m >>>>>>>>>>>> Install Nodejs <<<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
-  dnf install nodejs -y &>>${log}
-  func_exit_status
-
-  func_apppreq
-
-  echo -e "\e[36m >>>>>>>>>>> Download nodejs dependencies <<<<<<<<<<<<<<\e[0m" | tee -a ${log}
-  npm install &>>${log}
-  func_exit_status
-
-  func_schema_setup
-
-  func_systemd
-}
-
-func_systemd () {
-
-  echo -e "\e[36m >>>>>>>>>>>> Restart the ${component} service <<<<<<<<<<<<<<<\e[0m" | tee -a ${log}
+func_systemd() {
+  echo -e "\e[36m>>>>>>>>>>>>  Start ${component} Service  <<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
   systemctl daemon-reload &>>${log}
   systemctl enable ${component} &>>${log}
-  systemctl start ${component} &>>${log}
+  systemctl restart ${component} &>>${log}
   func_exit_status
 }
 
 func_schema_setup() {
-
   if [ "${schema_type}" == "mongodb" ]; then
-    echo -e "\e[36m >>>>>>>>>>> Install Mongodb client <<<<<<<<<<<<<<\e[0m" | tee -a ${log}
-    dnf install mongodb-org-shell -y &>>${log}
+    echo -e "\e[36m>>>>>>>>>>>>  INstall Mongo Client  <<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
+    yum install mongodb-org-shell -y &>>${log}
     func_exit_status
 
-    echo -e "\e[36m >>>>>>>>>>>>> Load the schema <<<<<<<<<<<<\e[0m" | tee -a ${log}
+    echo -e "\e[36m>>>>>>>>>>>>  Load User Schema  <<<<<<<<<<<<\e[0m"  | tee -a /tmp/roboshop.log
     mongo --host mongodb-dev.vinithaws.online </app/schema/${component}.js &>>${log}
     func_exit_status
   fi
@@ -99,6 +66,33 @@ func_schema_setup() {
     mysql -h mysql-dev.vinithaws.online -uroot -pRoboShop@1 < /app/schema/${component}.sql &>>${log}
     func_exit_status
   fi
+
+}
+
+func_nodejs() {
+  log=/tmp/roboshop.log
+
+  echo -e "\e[36m>>>>>>>>>>>>  Create MongoDB Repo  <<<<<<<<<<<<\e[0m"
+  cp mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
+  func_exit_status
+
+  echo -e "\e[36m>>>>>>>>>>>>  Install NodeJS Repos  <<<<<<<<<<<<\e[0m"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log}
+ func_exit_status
+
+  echo -e "\e[36m>>>>>>>>>>>>  Install NodeJS  <<<<<<<<<<<<\e[0m"
+  yum install nodejs -y &>>${log}
+  func_exit_status
+
+  func_apppreq
+
+  echo -e "\e[36m>>>>>>>>>>>>  Download NodeJS Dependencies  <<<<<<<<<<<<\e[0m"
+  npm install &>>${log}
+  func_exit_status
+
+  func_schema_setup
+
+  func_systemd
 }
 
 func_java() {
@@ -125,6 +119,7 @@ func_python() {
 
   func_apppreq
 
+  sed -i "s/rabbitmq_app_password/${rabbitmq_app_password}/" /etc/systemd/system/${component}.service
 
   echo -e "\e[36m>>>>>>>>>>>>  Build ${component} Service   <<<<<<<<<<<<\e[0m"
   pip3.6 install -r requirements.txt &>>${log}
@@ -132,4 +127,3 @@ func_python() {
 
   func_systemd
 }
-
