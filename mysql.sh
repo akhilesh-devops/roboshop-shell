d7f4d9beb07f97f49be71011e561aef9fa1ce6b7
AKhilesh <kothapallyakhil123@gmail.com>
2025-01-14 07:20:13 +0530

    test

--- mysql.sh
+++ mysql.sh
@@ -1,5 +1,11 @@
 source common.sh
 
+mysql_root_password=$1
+if [ -z "${mysql_root_password}" ]; then
+  echo "Input Password is Missing"
+  exit 1
+fi
+
 echo -e "\e[36m >>>>>>>>>>> create mysql repo <<<<<<<<<<<\e[0m" | tee -a ${log}
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>${log}
 func_exit_status
@@ -21,5 +27,5 @@ systemctl restart mysqld &>>${log}
 func_exit_status
 
 echo -e "\e[36m >>>>>>>>>>> Set password <<<<<<<<<<<<<<\e[0m"
-mysql_secure_installation --set-root-pass RoboShop@1 &>>${log}
+mysql_secure_installation --set-root-pass ${mysql_root_password} &>>${log}
 func_exit_status
